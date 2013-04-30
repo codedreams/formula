68ea7da2480a3e07e0d75f3bebd3b84671d90d62
miked <mikemetamac@gmail.com>
2013-04-29 22:31:18 -0400

    work on validation and tests

--- src/formula/validation.clj
+++ src/formula/validation.clj
@@ -1,8 +1,5 @@
 (ns formula.validation)
 
-(def user {:username "hello" :password "abc" :confirmation "abc"
-           :gender nil})
-
 (def call
   "Changes strings into function calls"
   #(resolve (symbol (name %))))
@@ -35,8 +32,10 @@
   [field-key answer vali-map & [messages]]
   (let [custom (:allow-blank (field-key messages))
         value (field-key vali-map)
+        value (if (string? value) (count (clojure.string/trim value)) value)
+        value (if (nil? value) 1 value)
         match (when-not answer
-                (= 0 (count (clojure.string/trim value))))]
+                (= 0 value))]
     (when match
       {field-key (message field-key custom "%s can't be blank")})))
 
@@ -85,36 +84,41 @@
   [field-key length-map vali-map & [messages]]
   (let [min (length-map :min)
         max (length-map :max)
-        value (field-key vali-map)
+        both-values (not-any? nil? [min max])
+        both-empty (and (nil? max) (nil? min))
+        value (count (field-key vali-map))
         custom (:length (field-key messages))
         result (cond
-                (not-any? nil?
-                          [min max]) (when-not (some #{(count value)}
-                                                     (range min max))
-                                       (str "%s should be between "
-                                            min " and " max
-                                            " characters"))
-                          (nil? max) (when-not (>= (count value) min)
-                                       (str "%s should be at least"
-                                            min "characters"))
-                          (nil? min) (when-not (<= (count value) max)
-                                       (str "%s should be at most"
-                                            max "characters")))]
-
-    {field-key (message field-key custom result)}))
-
-(length :username {:min 30 :max 17} user)
-
+                both-empty nil
+                both-values (when-not (some #{value}
+                                            (range min (+ 1 max)))
+                              (str "%s should be between "
+                                   min " and " max
+                                   " characters"))
+                (nil? max) (when-not (>= value min)
+                             (str "%s should be at least "
+                                  min " characters"))
+                (nil? min) (when-not (<= value max)
+                             (str "%s should be at most "
+                                  max " characters")))]
+    (when result
+      {field-key (message field-key custom result)})))
 
 (defn unique
   "Checks to see if value is unique.  check-fn should be a function
-   that takes the value as an argument."
-  [field-key check-fn vali-map & [messages]])
+   that takes the value as an argument.  It should return true if not
+   unique. e.g. record in database will be true"
+  [field-key check-fn vali-map & [messages]]
+  (let [value (field-key vali-map)
+        result (check-fn value)
+        custom (:unique (field-key messages))]
+    (when result
+      {field-key (message field-key custom "%s must be unique")})))
 
-(defn- sender-loop [field rules vali-map & [messages]]
-  (let [vali #(if (map? %)
-                ((call (apply key %)) field (apply val %) vali-map %2)
-                ((call %) field vali-map %2))]
+(defn sender-loop [field rules vali-map & [messages]]
+  (let [vali (fn [r & [msg]] (if (map? r)
+                      ((call (apply key r)) field (apply val r) vali-map msg)
+                      ((call r) field vali-map msg)))]
         (loop [rules rules errors {}]
           (if (or (empty? rules) (seq errors))
             errors
@@ -126,56 +130,32 @@
   [field rules vali-map & [messages]]
   (let [allow-nil (some #{:allow-nil} rules)
         allow-blank (some #{:allow-blank} rules)
-        rules [(first (filter #{:present} rules))
-               {:allow-nil allow-nil}
-               {:allow-blank allow-blank}]]
+        present (first (filter #{:present} rules))
+        rules [{:allow-nil allow-nil}
+               {:allow-blank allow-blank}]
+        rules (if present (cons present rules) rules)]
     (sender-loop field rules vali-map messages)))
 
-;;; make automatic check for allow-nil and allow-blank
 (defn sender 
   "Sends rules to correct functions. Once an validation error has
    occurred for a specific field, then the rest of the rules are
    ignored."
   [field rules vali-map & [messages]]
-  (let [check (default-check field rules vali-map (field messages))
+  (let [check (default-check field rules vali-map messages)
         no-errors (empty? check)
         rules (when no-errors
                 (remove #{:allow-nil :allow-blank :present} rules))]
     (if no-errors
-      (sender-loop field rules vali-map (field messages))
+      (sender-loop field rules vali-map messages)
       check)))
 
-;(sender :password [:present {:formats #"\d+"}] user {:username "bad"})
-
-
-;;; make custom messages more customized, by including value in string
-;;; add something to make to help customize, and then remove after conj
-;;; idea - return key and vector, then another function to conj
-
-;;; add two functions for allow-nil, and allow-blank in map
-;;; should take allow-nil and allow-blank map last
 (defn validate 
   "Takes rules vector, to be validated map, and custom messages (optional).
    Each field's rules are ran until an validation error occurs."
   [rule-vec vali-map & [messages]]
   (let [errors (for [[field & rules] rule-vec
                      :let [result (sender field rules vali-map messages)]]
-                 result)
-        ;errors (into {} errors)
-        ]
-    (into {} errors)
-    ;(conj errors (select-keys messages (keys errors)))
-    ))
+                 result)]
+    (into {} errors)))
 
 
-(validate [[:password :present {:formats #"\d+"}]
-           [:username :present]
-           [:gender :present :allow-blank]
-           [:confirmation :present {:confirm [:password]}]
-           ;[:password :present {:length {:min 3 :max 5}}]
-           ]
-          user {:username {:present "bad"
-                           :allow-nil "%s can't be nil"}})
-
-;; {:password {:present "The password must be present"
-;;             :format "The format is wrong for password"}}
--- test/formula/validation_test.clj
+++ test/formula/validation_test.clj
@@ -23,7 +23,7 @@
 
 (def user-m {:username "joe" :password "abcdef" :confirmation "abcdef"})
 
-(fact "call - should take string and make function call"
+(facts "call - should take string and make function call"
       ((call "str") 1) => "1"
       ((call "nil?") nil) => true)
 
@@ -33,7 +33,7 @@
        (fact "with custom should return custom message"
              (message :excited "I am so %s" "nada") => "I am so excited"))
 
-(fact "present - should be nil or validation error with message"
+(facts "present - should be nil or validation error with message"
       (fact "error when not present"
             (present :nope user-m) => {:nope "nope must be present"})
       (fact "nil when present"
@@ -42,10 +42,12 @@
             (present :nope user-m {:nope {:present "where is %s"}})
             =>{:nope "where is nope"}))
 
-(fact "allow-nil - should be nil or validation error with message"
+(facts "allow-nil - should be nil or validation error with message"
       (let [user-nil (conj user-m {:username nil})]
         (fact "return nil when not nil"
               (allow-nil :username false user-m) => nil)
+        (fact "return nil with truthy value"
+              (allow-nil :username :t user-m) => nil)
         (fact "return nil when allow-nil is true"
               (allow-nil :username true user-nil) => nil)
         (fact "error when nil"
@@ -55,20 +57,23 @@
               (allow-nil :username false user-nil
                          {:username {:allow-nil "%s shouldn't be nil"}}))))
 
-(fact "allow-blank - should be nil or validation error with message"
+(facts "allow-blank - should be nil or validation error with message"
       (let [user-blank (conj user-m {:username ""})]
         (fact "return nil when not blank"
               (allow-blank :username false user-m) => nil)
         (fact "return nil when allow-blank is true"
               (allow-blank :username true user-blank) => nil)
+        (fact "return nil when value is nil"
+              (allow-blank :username false (conj user-m {:username nil}))
+              => nil)
         (fact "error when blank"
-              (allow-blank :username false user-blank)
+              (allow-blank :username nil user-blank)
               => {:username "username can't be blank"})
         (fact "show custom message when provided"
               (allow-blank :username false user-blank
                          {:username {:allow-nil "%s shouldn't be nil"}}))))
 
-(fact "confirm - should be nil or validation error with message"
+(facts "confirm - should be nil or validation error with message"
       (let [same-values (conj user-m {:username "abcdef"})]
         (fact "return nil when confirm matches"
               (confirm :confirmation [:password] user-m) => nil)
@@ -86,7 +91,7 @@
                                                          {:confirm "no match"}})
               => {:confirmation "no match"})))
 
-(fact "exclusion - should be nil or validation error with message"
+(facts "exclusion - should be nil or validation error with message"
       (fact "return nil when not in coll"
             (exclusion :username ["hi" "you"] user-m) => nil)
       (fact "error when in coll"
@@ -97,7 +102,7 @@
                                                 {:exclusion "in list"}})
             => {:username "in list"}))
 
-(fact "inclusion - should be nil or validation error with message"
+(facts "inclusion - should be nil or validation error with message"
       (fact "return nil when in coll"
             (inclusion :username ["who" "joe"] user-m) => nil)
       (fact "error when not in coll"
@@ -108,7 +113,7 @@
                                                 {:inclusion "where's the %s"}})
             => {:username "where's the username"}))
 
-(fact "formats - should be nil or validation error with message"
+(facts "formats - should be nil or validation error with message"
       (let [user-reg (conj user-m {:username "384"})]
         (fact "return nil when matches regex"
               (formats :username #"\d+" user-reg) => nil)
@@ -121,22 +126,122 @@
                                                  :formats "no match"}})
               => {:username "no match"})))
 
-(fact "length - should be nil or validation error with message"
+(facts "length - should be nil or validation error with message"
       (fact "return nil when the correct length"
             (length :username {:min 3 :max 8} user-m) => nil)
+      
       (fact "error when not correct length"
             (length :username {:min 8 :max 20} user-m)
             {:username "username should be between 8 and 20 characters"})
+      
       (fact "error with only min"
-            (length :username {:min 3} user-m)
-            => {:username "username should be at least 3 characters"})
+            (length :username {:min 4} user-m)
+            => {:username "username should be at least 4 characters"})
+      
       (fact "error with only max"
-            (length :username {:max 8} user-m)
-            => {:username "username should be at most 3 characters"})
+            (length :username {:max 2} user-m)
+            => {:username "username should be at most 2 characters"})
+      
       (fact "show custom error message when provided"
-            (length :username {:max 8} user-m {:username
-                                               {:length "gotta be long"}})
-            => {:username "gotta be long"}))
+            (length :username {:max 2} user-m {:username
+                                               {:length "gotta be longer"}})
+            => {:username "gotta be longer"})
+      (fact "when both present but error with min"
+            (length :username {:min 8 :max 20} user-m)
+            => {:username "username should be between 8 and 20 characters"})
+      (fact "when both present but error with max"
+            (length :username {:min 1 :max 2} user-m)
+            => {:username "username should be between 1 and 2 characters"}))
+
+(facts "unique - should be nil or validation error with message"
+      (fact "return nil when value is unique"
+            (unique :username #(= % "joe") user-m)
+            => {:username "username must be unique"})
+      (fact "error when value is not unique"
+            (unique :username #(= % "joe") (conj user-m {:username "bob"})) => nil)
+      (fact "custom message when provided"
+            (unique :username #(= % "joe") user-m {:username
+                                                   {:unique "not unique"}})
+            => {:username "not unique"}))
+
+(facts "sender-loop vali function - should direct rule"
+      (let [vali (fn [r & msg] (if (map? r)
+                       ((call (apply key r)) :username (apply val r) user-m msg)))]
+        (fact "should return nil"
+              (vali :present ) => nil)
+        (fact "should return error"
+              (vali {:unique #(= % "joe")} {})
+              => {:username "username must be unique"})
+        (fact "should return nil"
+              (vali {:length {:min 2 :max 5}} {}))))
+
+(facts "sender-loop loop"
+      (let [vali (fn [r & msg] (if (map? r)
+                          ((call (apply key r)) :username (apply val r) user-m msg)
+                          ((call r) :username user-m msg)))
+            send-loop (fn [rules & msg] (loop [rules rules errors {}]
+                                    (if (or (empty? rules) (seq errors))
+                                      errors
+                                      (recur (rest rules)
+                                             (conj errors (vali (first rules)
+                                                                msg))))))]
+        (fact "should return nil"
+              (send-loop [:present]) => {})
+        (fact "should return present error"
+              (send-loop [:present {:unique #(= % "joe")}]) => {:username
+                                                       "username must be unique"})))
+
+(facts "sender-loop - should return errors or {}"
+      (fact "should return {} when all is good"
+            (sender-loop :username [:allow-nil] user-m) => {})
+      (fact "should be return unique error"
+            (sender-loop :username [:present {:unique #(= % "joe")}] user-m)
+            => {:username "username must be unique"})
+      (fact "should return custom error when provided"
+            (sender-loop :username [{:formats #"\d+"}] user-m {:username
+                                                               {:formats "bad"}})
+            => {:username "bad"})
+      (fact "should only return the first error"
+            (sender-loop :username [{:unique #(= % "joe")} {:formats #"\d+"}]
+                         user-m) => {:username "username must be unique"}))
 
 
+(facts "default-check - should return errors or {}"
+       (fact "should return {} when all is good"
+             (default-check :username [:allow-nil] user-m) => {})
+       (fact "should return an error when nil"
+             (default-check :username [:present] (conj user-m {:username nil}))
+             => {:username "username can't be empty"})
+       (fact "should return an error when empty?"
+             (default-check :username [nil :present] (conj user-m {:username ""}))
+             => {:username "username can't be blank"})
+       (fact "nil should work if allow-nil is in rules"
+             (default-check :username [:present :allow-nil]
+               (conj user-m {:username nil})) => {}))
+
+
+(facts "sender - should return errors or {}"
+      (fact "should return {} when all is good"
+            (sender :username [:present] user-m) => {})
+      (fact "should return {} when all is good"
+            (sender :username [:allow-nil] user-m) => {})
+      (fact "error when wrong format"
+            (sender :username [:present {:formats #"\d+"}] user-m)
+            => {:username "username is not the correct format"})
+      (fact "custom message should show when provided"
+            (sender :username [:present {:formats #"\d+"}] user-m
+                    {:username {:formats "wrong"}})
+            => {:username "wrong"}))
+
+
+(facts "validate - should return errors or {}")
+
+;; (validate [[:password :present {:formats #"\d+"}]
+;;            [:username :present]
+;;            [:gender :present :allow-blank]
+;;            [:confirmation :present {:confirm [:password]}]
+;;            ;[:password :present {:length {:min 3 :max 5}}]
+;;            ]
+;;           user {:username {:present "bad"
+;;                            :allow-nil "%s can't be nil"}})
 
