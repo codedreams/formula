e7dc5ef581571c10cdbc9d0515fb4e777f8785ca
miked <mikemetamac@gmail.com>
2013-05-02 02:51:21 -0400

    switch clojure versions

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/codedreams/formula"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [hiccup "1.0.3"]
                  [trust "1.0.0-beta"]])
